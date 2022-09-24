4915c99e7a1ebb694644084ada0961a6fab7216b
allen <eepson123@gmail.com>
2022-09-24 13:59:23 +0800

    feature:Add the loginPage init some UI

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -7,16 +7,36 @@
         alt="logo"
         class="logo__img"
       />
-      <h2 class="logo__text">Women<br />Techmakers</h2>
+      <h2 class="logo__text text-grey">Women<br />Techmakers</h2>
     </div>
     <div class="feature">
-      <v-btn color="indigo" variant="outlined" class="feature__btn">
+      <v-btn
+        size="large"
+        variant="outlined"
+        color="grey lighten-4"
+        class="feature__btn mx-auto mb-3"
+        max-width="270px"
+      >
         <template #default>
           <div class="google"></div>
-          Sign up with Google</template
-        >
+          <p class="font-weight-bold grey">Sign up with Google</p>
+        </template>
       </v-btn>
-      <v-btn color="primary"> Sign up</v-btn>
+      <v-btn
+        style="margin-bottom: 18px"
+        size="large"
+        min-width="253px"
+        color="primary"
+        class="mx-auto font-weight-bold"
+      >
+        Sign up</v-btn
+      >
+      <div class="info pl-3">
+        <p class="">
+          Already have an account ?
+          <a href="#" class="red--text">Sign in here.</a>
+        </p>
+      </div>
     </div>
   </main>
 </template>
@@ -30,27 +50,44 @@
   width: 20px;
   height: 20px;
 }
+
+.login {
+  // padding: 13px;
+  max-width: 320px;
+  margin: 0 auto;
+}
+
 .logo {
   &__img {
-    width: 6.4rem;
-    height: 6.4rem;
+    width: 8.5rem;
+    height: 8.5rem;
     display: block;
     margin: 0 auto;
   }
   &__text {
     text-align: center;
+    font-size: 2rem;
+    line-height: 125%;
   }
   margin-bottom: 66px;
 }
 .feature {
   display: flex;
   flex-wrap: wrap;
+
   &__btn {
-    margin: 0 auto;
+    position: relative;
   }
   .google {
-    position: relative;
-    left: -5px;
+    position: absolute;
+    left: 0px;
+    z-index: -1;
+  }
+}
+.info {
+  font-size: 8px;
+  a {
+    color: #5186ec;
   }
 }
 </style>
